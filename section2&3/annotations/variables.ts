afdc6ef2d7fa8fc62efacc3776e29c92c46cd8c7
Victoria Usan <victoria.usan@gmail.com>
2024-11-10 15:50:22 +0100

    Learning annotations

--- section2&3/annotations/variables.ts
+++ section2&3/annotations/variables.ts
@@ -7,3 +7,50 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+	x: 10,
+	y: 20,
+};
+
+// functions
+const logNumber: (i: number) => void = (i: number) => {
+	console.log(i);
+};
+
+// When to use annotations
+// 1) function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) we declare a variable on one line and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (const word of words) {
+	if (word === "green") {
+		foundWord = true;
+	}
+}
+
+// 3) we have a variable which type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (const number of numbers){
+    if (number > 0){
+        numberAboveZero = number;
+    }
+}
