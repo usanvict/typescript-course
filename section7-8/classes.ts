73aacdd30be6a51b96a6765c867bbebb7fefa78a
Victoria Usan <victoria.usan@gmail.com>
2024-11-21 13:59:09 +0100

    Finishing classes

--- section7-8/classes.ts
+++ section7-8/classes.ts
@@ -1,13 +1,68 @@
+// Uncomment to see public, private and protected methods
+
+// class Vehicle {
+// 	public drive(): void {
+// 		console.log("chugga chugga");
+// 	}
+
+// 	protected honk(): void {
+// 		console.log("beep");
+// 	}
+// }
+
+// class Car extends Vehicle {
+// 	private drive(): void {
+// 		console.log("wroom");
+// 	}
+
+// 	startDrivingProcess(): void {
+// 		this.drive();
+// 		this.honk();
+// 	}
+// }
+
+// const vehicle = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
+
+// const car = new Car();
+// car.startDrivingProcess();
+// car.honk();
+
 class Vehicle {
-	drive(): void {
-		console.log("chugga chugga");
-	}
+	// color: string;
+
+	// constructor(color: string) {
+	// 	this.color = color;
+	// }
+
+	constructor(public color: string) {}
 
 	honk(): void {
 		console.log("beep");
 	}
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+class Car extends Vehicle {
+	constructor(
+		public wheels: number,
+		color: string,
+	) {
+		super(color);
+	}
+
+	drive(): void {
+		console.log("wroom");
+	}
+
+	startDrivingProcess(): void {
+		this.drive();
+		this.honk();
+	}
+}
+
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
+const car = new Car(5, "red");
+console.log(`${car.color} ${car.wheels}`);
